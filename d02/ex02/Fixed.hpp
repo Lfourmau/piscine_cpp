6c871136104fb92df9f83158f038f3c0ea6e634d
Lfourmau <loicfourmaux@gmail.com>
2021-11-26 14:16:18 +0100

    d2ex02 seems good

--- d02/ex02/Fixed.hpp
+++ d02/ex02/Fixed.hpp
@@ -19,16 +19,24 @@ class Fixed {
 		bool operator<=(Fixed const& rhs);
 		bool operator==(Fixed const& rhs);
 		bool operator!=(Fixed const& rhs);
-		Fixed& operator=(Fixed const& rhs);
-		Fixed operator+(Fixed const& rhs);
-		Fixed& operator-(Fixed const& rhs);
+		Fixed& operator+(Fixed const& rhs);
 		Fixed& operator*(Fixed const& rhs);
+		Fixed& operator-(Fixed const& rhs);
+		Fixed& operator=(Fixed const& rhs);
 		Fixed& operator/(Fixed const& rhs);
+		Fixed operator++(int);
+		Fixed& operator++(void);
+		Fixed operator--(int);
+		Fixed& operator--(void);
 		//functions
 		int 	getRawBits(void) const;
 		void 	setRawBits(int const raw);
 		float 	toFloat(void) const;
 		int 	toInt(void) const;
+		static Fixed& min(Fixed& val1, Fixed& val2);
+		static Fixed& max(Fixed& val1, Fixed& val2);
+		static Fixed const& min(Fixed const& val1, Fixed const& val2);
+		static Fixed const& max(Fixed const& val1, Fixed const& val2);
 
 
 	private :
--- d02/ex02/main.cpp
+++ d02/ex02/main.cpp
@@ -3,24 +3,14 @@
 
 int main(void)
 {
-	Fixed a(89);
-	Fixed const b(78);
-	bool test;
-	Fixed addition(a + b);
-
-	test = a < b;
-	std::cout << "< "<< test << std::endl;
-	test = a > b;
-	std::cout << "> "<< test << std::endl;
-	test = a <= b;
-	std::cout << "<= "<< test << std::endl;
-	test = a >= b;
-	std::cout << "<= "<< test << std::endl;
-	test = (a == b);
-	std::cout << "== "<< test << std::endl;
-	test = (a != b);
-	std::cout << "!= "<< test << std::endl;
-	std::cout << "+" << addition << std::endl;
-	
+	Fixed a;
+	Fixed const b(Fixed(5.05f) * Fixed(2));
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max(a, b) << std::endl;
 	return 0;
 }
