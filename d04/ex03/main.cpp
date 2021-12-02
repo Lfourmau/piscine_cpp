4b09efc022ad5f07b1aa24a8f644eb8e22e016b4
Loic Fourmaux <lfourmaux@birdyx.fr>
2021-12-02 18:44:35 +0100

    just re-setup the main for subject tests

--- d04/ex03/main.cpp
+++ d04/ex03/main.cpp
@@ -6,22 +6,24 @@
 
 int main()
 {
-	//IMateriaSource* src = new MateriaSource();
-	//src->learnMateria(new Ice());
-	//src->learnMateria(new Cure());
-	//ICharacter* me = new Character("me");
-	//AMateria* tmp;
-	//tmp = src->createMateria("ice");
-	//me->equip(tmp);
-	//tmp = src->createMateria("cure");
-	//me->equip(tmp);
-	//ICharacter* bob = new Character("bob");
-	//me->use(0, *bob);
-	//me->use(1, *bob);
-	//delete bob;
-	//delete me;
-	//delete src;
-	Ice materia;
+	std::cout << "Subject tests" << std::endl;
+	IMateriaSource* src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+	ICharacter* me = new Character("me");
+	AMateria* tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	ICharacter* bob = new Character("bob");
+	me->use(0, *bob);
+	me->use(1, *bob);
+	delete bob;
+	delete me;
+	delete src;
+	std::cout << "Clone test" << std::endl;
+	Cure materia;
 	std::cout << materia.getType() << std::endl;
 	AMateria *test;
 	test = materia.clone();
