d211dc32a286c40482f9e58c73933b7f63dde359
Loic Fourmaux <lfourmaux@birdyx.fr>
2021-12-09 08:48:28 +0100

    Ex02 done

--- d06/ex02/main.cpp
+++ d06/ex02/main.cpp
@@ -29,77 +29,53 @@ Base * generate(void)
 
 void identify(Base * p)
 {
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
+
+void identify(Base& p)
+{
+	Base b;
 	try
 	{
-		A* Aptr = dynamic_cast<A*>(p);
-		if (Aptr)
-			std::cout << "A" << std::endl;
+		b = dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
 	}
 	catch(const std::bad_cast& e)
 	{
-		std::cerr << e.what() << '\n';
-	}
-	
-	try
-	{
-		B* Bptr = dynamic_cast<B*>(p);
-		if (Bptr)
+		try
+		{
+			b = dynamic_cast<B&>(p);
 			std::cout << "B" << std::endl;
-	}
-	catch(const std::bad_cast& e)
-	{
-		std::cerr << e.what() << '\n';
+		}
+		catch(const std::bad_cast& e)
+		{
+			try
+			{
+				b = dynamic_cast<C&>(p);
+				std::cout << "C" << std::endl;
+			}
+			catch(const std::bad_cast& e)
+			{
+				std::cerr << e.what() << '\n';
+			}
+		}
 	}
 
-	try
-	{
-		C* Cptr = dynamic_cast<C*>(p);
-		if (Cptr)
-			std::cout << "C" << std::endl;
-	}
-	catch(const std::bad_cast& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	
 }
 
 int main(void)
 {
-	Base *test = generate();
-	identify(test);
-	delete test;
-	test = generate();
-	identify(test);
-	delete test;
-	test = generate();
-	identify(test);
-	delete test;
-	test = generate();
-	identify(test);
-	delete test;
-	test = generate();
-	identify(test);
-	delete test;
-	test = generate();
-	identify(test);
-	delete test;
-	test = generate();
-	identify(test);
-	delete test;
-	test = generate();
-	identify(test);
-	delete test;
-	test = generate();
-	identify(test);
-	delete test;
-	test = generate();
-	identify(test);
-	delete test;
-	test = generate();
-	identify(test);
-	delete test;
-	test = generate();
-	identify(test);
-	delete test;
+	Base *test;
+	for (size_t i = 0; i < 40; i++)
+	{
+		test = generate();
+		identify(test);
+		identify(*test);
+		delete test;
+	}
 }
