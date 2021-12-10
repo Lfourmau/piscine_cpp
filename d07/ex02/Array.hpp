a67585bd0805671892ba5501796950789d8fb2f4
Loic Fourmaux <lfourmaux@birdyx.fr>
2021-12-10 17:42:11 +0100

    ex02 done

--- d07/ex02/Array.hpp
+++ d07/ex02/Array.hpp
@@ -7,24 +7,31 @@ template <typename T>
 class Array {
 	public:
 		T *array;
+		size_t length;
 
-		Array(void) {
+		Array(void) : length(0) {
 			this->array = NULL;
 		}
 		~Array(void) {};
 		Array(unsigned int n) {
 			std::cout << "N constructor : " << n << std::endl;
-			this->array = new T[n];
-			for (size_t i = 0; i < n; i++)
+			length = n;
+			if (n > 0)
 			{
-				this->array[i] = T();
-				std::cout << "default = " << this->array[i] << ";" << i << std::endl;
+				this->array = new T[n];
+				for (size_t i = 0; i < n; i++)
+				{
+					this->array[i] = T();
+					std::cout << "default = " << this->array[i] << ";" << i << std::endl;
+				}
 			}
+			else
+				this->array = NULL;
 			
 		}
 		size_t size(void) const
 		{
-			return (30);
+			return (length);
 		}
  		T& operator[](size_t idx)
 		{
@@ -32,13 +39,13 @@ class Array {
 			{
 				if (this->array == NULL || idx > this->size() || idx < 0)
 					throw(std::exception());
+				return (this->array[idx]);
 			}
 			catch(const std::exception& e)
 			{
 				std::cout << idx << "/" << this->size() << " Out of lmits" << std::endl;
-				return *this->array;
+				return (*this->array);
 			}
-			return (this->array[idx]);
 			
 		}
 		Array(Array const &src) {
@@ -46,8 +53,9 @@ class Array {
 		}
 		Array& operator=(Array const& rhs)
 		{
-			delete []array;
+			//delete []array;
 			this->array = new T[rhs.size()];
+			this->length = rhs.length;
 			for (size_t i = 0; i < rhs.size(); i++)
 				this->array[i] = rhs.array[i];
 			return (*this);
